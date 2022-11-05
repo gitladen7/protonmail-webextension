af6c1dbeeaf70b2be93cbbde790f32369d1d3aa1
Scallywer <djzerorts@gmail.com>
2022-11-05 15:01:25 +0100

    Update protonDomains.js

--- src/shared/protonDomains.js
+++ src/shared/protonDomains.js
@@ -1,9 +1,6 @@
 module.exports = {
     protonDomains: [
-        "mail.protonmail.com",
-        "old.protonmail.com",
-        "beta.protonmail.com",
-        "app.protonmail.ch",
+        "mail.proton.me",
         "protonirockerxow.onion",
     ],
 };
